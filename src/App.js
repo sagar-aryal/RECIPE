264918e381b3cf0df74760bdf94860b677541844
sagar-aryal <sagar_yal92@outlook.com>
2021-10-28 21:20:45 +0300

    fix routing error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { HashRouter } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HashRouter basename="/">
+      <BrowserRouter>
         <Header />
         <Main />
         <Footer />
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
